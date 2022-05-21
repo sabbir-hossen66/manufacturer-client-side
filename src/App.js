f6319fc3b24ffced1ba0b7f9ab40e91b2a6ba8ee
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-05-22 02:47:36 +0600

    title added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Title from './Pages/Home/Title/Title';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>hellow</h2>
+      <Title></Title>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}>Home</Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div>
+            <marquee behavior="" direction="right"><h1 className='text-purple-400 font-serif font-bold text-3xl'>Dwell Computer Factory</h1></marquee>
+        </div>
+    );
+};
+
+export default Title;
